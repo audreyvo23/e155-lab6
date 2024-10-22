d3169da5e7ea61f117ffbe64d43d3f9f572ced69
audreyvo23 <107438594+audreyvo23@users.noreply.github.com>
2024-10-21 21:33:58 -0700

    Add files via upload

--- STM32L432KC_SPI.c
+++ STM32L432KC_SPI.c
@@ -15,23 +15,27 @@
  * Refer to the datasheet for more low-level details. */ 
  
 void initSPI(int br, int cpol, int cpha) {
-  SPI1->CR1 |= (0b010 << SPI_CR1_BR_Pos); // divide by 8
+  RCC->APB2ENR |= (RCC_APB2ENR_SPI1EN); // give SPI clock 80 Mhz
+
+  SPI1->CR1 |= (0b110 << SPI_CR1_BR_Pos); // divide by 128
+
   SPI1->CR1 |= _VAL2FLD(SPI_CR1_CPOL, cpol); 
   SPI1->CR1 |= _VAL2FLD(SPI_CR1_CPHA, cpha);
-  SPI1->CR1 |= _VAL2FLD(SPI_CR1_RXONLY, 0b0);
+
+  //SPI1->CR1 |= _VAL2FLD(SPI_CR1_RXONLY, 0b0);
   SPI1->CR1 |= _VAL2FLD(SPI_CR1_LSBFIRST, 0b0);
   SPI1->CR1 |= _VAL2FLD(SPI_CR1_CRCEN, 0b0);
   SPI1->CR1 |= _VAL2FLD(SPI_CR1_SSM, 0b1); // enable slave software
-   SPI1->CR1 |= _VAL2FLD(SPI_CR1_SSI, 0b1); // enable slave software
+  SPI1->CR1 |= _VAL2FLD(SPI_CR1_SSI, 0b1); // enable slave software
   SPI1->CR1 |= _VAL2FLD(SPI_CR1_MSTR, 0b1); // set to master
 
 
   SPI1->CR2 |= (0b0111 << SPI_CR2_DS_Pos); // set data size to 8 bits
   SPI1->CR2 |= _VAL2FLD(SPI_CR2_SSOE, 0b1);
   SPI1->CR2 |= _VAL2FLD(SPI_CR2_FRF, 0b0); // motorola mode
-  SPI1->CR2 |= _VAL2FLD(SPI_CR2_FRXTH, 0b1); // motorola mode
+  SPI1->CR2 |= _VAL2FLD(SPI_CR2_FRXTH, 0b1); 
 
-  SPI1->CR1 |= _VAL2FLD(SPI_CR1_SPE, 0b1); // set to master
+  SPI1->CR1 |= SPI_CR1_SPE; // enable spi
 
 }
 
@@ -42,7 +46,7 @@ char spiSendReceive(char send){
   while(!(SPI_SR_TXE & SPI1->SR)) {
   }
   //data is sent because it is written to data register
-  volatile uint8_t *draddress = &SPI1->DR;
+  volatile uint8_t* draddress = (volatile uint8_t*)&SPI1->DR;
   *draddress = send;
   // checks that data has been received
   while(!(SPI_SR_RXNE & SPI1->SR)) {
--- main.c
+++ main.c
@@ -10,6 +10,7 @@ Date: 10/18/2024
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
+#include "stm32l432xx.h"
 
 /////////////////////////////////////////////////////////////////
 // Provided Constants and Functions
@@ -23,6 +24,11 @@ char* webpageStart = "<!DOCTYPE html><html><head><title>E155 Web Server Demo Web
 char* ledStr = "<p>LED Control:</p><form action=\"ledon\"><input type=\"submit\" value=\"Turn the LED on!\"></form>\
 	<form action=\"ledoff\"><input type=\"submit\" value=\"Turn the LED off!\"></form>";
 char* webpageEnd   = "</body></html>";
+char* tempResol = "<p>Temperature Resolution:</p><form action=\"8bit\"><input type=\"submit\" value=\"8-bit Resolution\"></form>\
+	<form action=\"9bit\"><input type=\"submit\" value=\"9-bit Resolution\"></form>\
+        <form action=\"10bit\"><input type=\"submit\" value=\"10-bit Resolution\"></form>\
+        <form action=\"11bit\"><input type=\"submit\" value=\"11-bit Resolution\"></form>\
+        <form action=\"12bit\"><input type=\"submit\" value=\"12-bit Resolution\"></form>";
 
 //determines whether a given character sequence is in a char array request, returning 1 if present, -1 if not present
 int inString(char request[], char des[]) {
@@ -46,6 +52,39 @@ int updateLEDStatus(char request[])
 	return led_status;
 }
 
+int res_status = 8;
+int updateTempRes(char request[])
+{
+	// The request has been received. now process to determine whether to turn the LED on or off
+	if (inString(request, "8bit")==1) {
+		res_status = 8;
+	}
+	else if (inString(request, "9bit")==1) {
+		res_status = 9;
+	}
+        else if (inString(request, "10bit")==1) {
+		res_status = 10;
+	}
+
+        else if (inString(request, "11bit")==1) {
+		res_status = 11;
+	}
+
+        else if (inString(request, "12bit")==1) {
+		res_status = 12;
+	}
+
+	return res_status;
+}
+
+// Function used by printf to send characters to the laptop
+int _write(int file, char *ptr, int len) {
+  int i = 0;
+  for (i = 0; i < len; i++) {
+    ITM_SendChar((*ptr++));
+  }
+  return len;
+}
 /////////////////////////////////////////////////////////////////
 // Solution Functions
 /////////////////////////////////////////////////////////////////
@@ -64,7 +103,7 @@ int main(void) {
   RCC->APB2ENR |= (RCC_APB2ENR_TIM15EN);
   initTIM(TIM15);
 
-  RCC->APB2ENR |= (RCC_APB2ENR_SPI1EN);
+ 
   
   USART_TypeDef * USART = initUSART(USART1_ID, 125000);
 
@@ -88,7 +127,7 @@ int main(void) {
     Requests take the form of '/REQ:<tag>\n', with TAG begin <= 10 characters.
     Therefore the request[] array must be able to contain 18 characters.
     */
-/*
+
     // Receive web request from the ESP
     char request[BUFF_LEN] = "                  "; // initialize to known value
     int charIndex = 0;
@@ -98,54 +137,74 @@ int main(void) {
       // Wait for a complete request to be transmitted before processing
       while(!(USART->ISR & USART_ISR_RXNE));
       request[charIndex++] = readChar(USART);
-    }
-*/
-    // TODO: Add SPI code here for reading temperature
+    } 
+    
+
+    // Add SPI code here for reading temperature
   
     // Update string with current LED state
    
     // spi write data 
-    char msb;
+    int msb;
     char lsb;
-    int temp;
-    // on
+    char resol;
+    res_status = updateTempRes(request);
+
+    // set the resolution
+    if(res_status == 8) resol = 0b11100000;
+    if(res_status == 9) resol = 0b11100010;
+    if(res_status == 10) resol = 0b11100100;
+    if(res_status == 11) resol = 0b11100110;
+    if(res_status == 12) resol = 0b11101000;
+    
     digitalWrite(PA8, PIO_HIGH);
     spiSendReceive(0x80);
-    spiSendReceive(0x22);
+    spiSendReceive(resol);
+   
     digitalWrite(PA8, PIO_LOW);
-    // off
-    /*delay_millis(TIM15, 100);
-    // on
     digitalWrite(PA8, PIO_HIGH);
     // spi read data
+
     spiSendReceive(0x02);
     msb = spiSendReceive(0x00);
+
     digitalWrite(PA8, PIO_LOW);
-    // off
-    // on
     digitalWrite(PA8, PIO_HIGH);
+
     spiSendReceive(0x01);
     lsb = spiSendReceive(0x00);
+
     digitalWrite(PA8, PIO_LOW);
-    // off
+
     
-    int allbits = (msb << 8) | lsb;
-    int tempdata= allbits & 0b01111111;
-    int newtempdata;
+    delay_millis(TIM15, 100);
+
+   
+    // msk msb bit
+    int mskmsb = msb & 0b01111111;
+    float newtempdata;
     
     //get sign bit
-    int signbit = allbits & (1 << 15);
-    if (signbit) { // check if positive
-    newtempdata = tempdata;
-      
+    int signbit = msb & (1 << 7);
+    if (!signbit) { // check if positive
+      newtempdata = mskmsb;
+      if(1 << 7 & lsb) newtempdata += 0.5;
+      if(1 << 6 & lsb) newtempdata += 0.25;
+      if(1 << 5 & lsb) newtempdata += 0.125;
+      if(1 << 4 & lsb) newtempdata += 0.0625; 
     }
-    else {
-    newtempdata = ~tempdata + 1;
+    else { // is negative
+      newtempdata = -(~mskmsb + 1);
+      if(1 << 7 & lsb) newtempdata -= 0.5;
+      if(1 << 6 & lsb) newtempdata -= 0.25;
+      if(1 << 5 & lsb) newtempdata -= 0.125;
+      if(1 << 4 & lsb) newtempdata -= 0.0625;
     }
 
-    
-
-  /*
+    printf("msb: %d \n", msb);
+    printf("lsb: %d \n", lsb);
+    printf("temp: %f \n", newtempdata);
+  
     int led_status = updateLEDStatus(request);
 
     char ledStatusStr[20];
@@ -154,7 +213,10 @@ int main(void) {
     else if (led_status == 0)
       sprintf(ledStatusStr,"LED is off!");
 
-
+    char temp[20];
+    char units[20];
+    sprintf(temp, "%f ", newtempdata);
+    sprintf(units,"degrees Celsius");
 
     // finally, transmit the webpage over UART
     sendString(USART, webpageStart); // webpage header code
@@ -167,8 +229,14 @@ int main(void) {
     sendString(USART, ledStatusStr);
     sendString(USART, "</p>");
 
-  
+    sendString(USART, "<h2>Current Temperature</h2>");
+    sendString(USART, "</p>");
+    sendString(USART, temp);
+    sendString(USART, units);
+    sendString(USART, "</p>");
+    sendString(USART, tempResol);
     sendString(USART, webpageEnd);
-    */
+    
+    
   }
 }
